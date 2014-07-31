2cfa50de1ab98e58880157a96e6b8da075e2f165
Will Gauvin <willgauvin@me.com>
2014-07-31 19:44:01 +1200

    Refactored the views example to a proper package structure

--- src/main/java/net/gauvins/view/controller/ViewExampleController.java
+++ src/main/java/net/gauvins/view/controller/ViewExampleController.java
@@ -1,4 +1,4 @@
-package net.gauvins.controller;
+package net.gauvins.view.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
